0956f344a0bb127eb8143de7c39f7b1aae7520ed
michel.beyrouty@washmen.com <Michel El Beyrouty>
2020-11-26 21:08:51 +0200

    [#5] Add gameSession.add controller

--- api/controllers/gameSession/add.js
+++ api/controllers/gameSession/add.js
@@ -4,20 +4,59 @@ module.exports = {
   description:  'Add game session',
 
   inputs: {
+    playerIdsList: { // In order
+      type:     ['string'],
+      required: true,
+    },
+    gameType: {
+      type:     'string',
+      required: true,
+      isIn:     Object.values(sails.config.custom.GAME_TYPES),
+    },
   },
 
   exits: {
-    serverError: { responseType: 'serverError' },
-    success:     { description: 'ok' },
+    duplicatePlayerInGame: { responseType: 'duplicatePlayerInGame' },
+    gameConfigNotFound:    { responseType: 'gameConfigNotFound' },
+    playerNotFound:        { responseType: 'playerNotFound' },
+    serverError:           { responseType: 'serverError' },
+    success:               { description: 'ok' },
   },
 
-  fn: function (inputs, exits) {
+  fn: async function (inputs, exits) {
     try {
+
+      // TODO: think of a way to prevent same requests
+
+      const gameSession = await _fetchGameSession(inputs);
+      await gameSession.validatePlayers();
+      const scoresMap = gameSession.computeScores();
+
+      const { gameSessionId } = await sails.helpers.models.firebase.gameSession.add.with({
+        ...inputs,
+        scoresMap,
+      });
+
+      for (playerId of gameSession.playerIdsList) {
+        await sails.helpers.models.firebase.player.addGameSession.with({ playerId, gameSessionId });
+      }
 
       exits.success(this.req.player);
 
     } catch (error) {
       switch (_.get(error, 'raw.code') || _.get(error, 'code'))  {
+
+        case 'playerNotFound':
+          exits.playerNotFound();
+          break;
+
+        case 'gameConfigNotFound':
+          exits.gameConfigNotFound();
+          break;
+
+        case 'duplicatePlayerInGame':
+          exits.duplicatePlayerInGame();
+          break;
 
         default:
           exits.serverError(error);
@@ -29,3 +68,14 @@ module.exports = {
   },
 };
 
+
+async function _fetchGameSession ({ gameType, playerIdsList }) {
+
+  const { minPlayers, maxPlayers, DeliminatorsMap, scoreMultiplier, maxScore }
+  = await sails.helpers.models.firebase.gameSessionConfig.getByType.with({ gameType });
+
+  const gameSession = new sails.classes.GameSession({
+    gameType, playerIdsList, maxScore, minPlayers, maxPlayers, DeliminatorsMap, scoreMultiplier });
+
+  return gameSession;
+}
--- config/custom.js
+++ config/custom.js
@@ -10,4 +10,9 @@ module.exports.custom = {
 
   HTTP_EXITS,
 
+  GAME_TYPES: {
+    CHESS:         'chess',
+    CATAN_CLASSIC: 'catan_classic',
+  },
+
 };
