618c3748ef932f080570c6d052a4a56c2430b0ef
michel.beyrouty@washmen.com <Michel El Beyrouty>
2020-11-30 01:32:32 +0200

    [quickFix] Fix deployment

--- config/env/production.js
+++ config/env/production.js
@@ -316,7 +316,7 @@ module.exports = {
     * (https://sailsjs.com/config/http)                                        *
     *                                                                          *
     ***************************************************************************/
-    // trustProxy: true,
+    trustProxy: true,
 
   },
 
