5c88e2c67779419bba61a1ab093f86258d66db0f
michel.beyrouty@washmen.com <Michel El Beyrouty>
2020-12-08 18:16:21 +0200

    [core] Fix cors options

--- config/security.js
+++ config/security.js
@@ -28,11 +28,12 @@ module.exports.security = {
   *                                                                          *
   ***************************************************************************/
 
-  // cors: {
-  //   allRoutes: false,
-  //   allowOrigins: '*',
-  //   allowCredentials: false,
-  // },
+  cors: {
+    allRoutes:           true,
+    allowOrigins:        '*',
+    allowCredentials:    false,
+    allowRequestHeaders: ['Authorization', 'content-type', 'manager-password', 'user-timestamp'],
+  },
 
 
   /****************************************************************************
